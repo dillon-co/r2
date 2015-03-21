4f30bc5b2e00bdde69c1377285a3a5fd9d857b10
Nicole Radford <nradford0171@gmail.com>
2015-03-21 16:48:46 -0600

    final profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :translators, dependent: :destroy
+  has_many :translations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150321194739) do
     t.datetime "updated_at"
   end
 
-  add_index "translations", ["user_id"], name: "index_translations_on_user_id", using: :btree
+  add_index "translators", ["user_id"], name: "index_translators_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
