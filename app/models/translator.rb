177a515cdc9ed8c564b5fb4448cfc36e3a4b3bfa
grandmasterspoc <dilloncortez@gmail.com>
2015-03-21 10:40:41 -0600

    added methods for translator

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -1,3 +1,15 @@
 class Translator < ActiveRecord::Base
   belongs_to :user
+
+  
+  def translator_key
+  end
+  
+  
+  def translated_audio(input)
+  end
+
+  
+  def origional_text
+  end   
 end
