c1aad5eb2ab199fa8293d70b1cd08cc0aa54137b
grandmasterspoc <dilloncortez@gmail.com>
2015-03-21 14:22:33 -0600

    added translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  root to: "translations#index"
+
   get 'users/show'
 
   get 'users/index'
 
   get 'users/profile'
-
-  resources :translations
-=======
->>>>>>> added translation
   
   resources :translations
 
   devise_for :users
   
-  root to: "translations#index"
-
 end
